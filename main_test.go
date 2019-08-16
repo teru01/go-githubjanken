53787dbf109cedf15b4592e85bce8fe0bfcdf21e
teruya <teru0x01.sheep@gmail.com>
2019-08-16 14:58:09 +0900

    add test

---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFetchContributions(t *testing.T) {
+	url := "https://github.com/users/teru01/contributions"
+	contri, err := fetchContributions(url)
+	if err != nil {
+		t.Error(err)
+	}
+	if contri != 602 {
+		t.Error("expected 602, got", contri)
+	}
+}
+
+func TestFetchContributionsZero(t *testing.T) {
+	url := "https://github.com/users/ksoo/contributions"
+	contri, err := fetchContributions(url)
+	if err != nil {
+		t.Error(err)
+	}
+	if contri != 0 {
+		t.Error("expected 0, got", contri)
+	}
+}
+
+func TestFetchError(t *testing.T) {
+	url := "https://github.com/_"
+	_, err := fetchContributions(url)
+	if err != nil {
+		switch err := err.(type) {
+		case *statusError:
+			if err.code != 404 {
+				t.Error("expected 404, got", err.code)
+			}
+			return
+		default:
+			t.Error(err)
+		}
+	}
+	t.Error("err did not returned")
+}
